8596e242b12ca95bcc069c836af69199aa327ff7
Lrd134 <larryc3200@gmail.com>
2021-03-15 14:14:33 -0400

    commit tree 8d9cda7d6870e8a663e035614bd89fcfc7aa5cc3

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
+  def count_sentences()
+    arr= []
+    self.split(" ").each do | ele |
 
+      if ele.end_with?(".") || ele.end_with?("!") || ele.end_with?("?")
+        arr << ele
+      end
+    end
+  arr.size
   end
+  
 end
